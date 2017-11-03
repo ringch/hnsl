0dfd16efbe2a2debd49666c71eb63d8e0cae3b5a
= <=>
2017-11-03 15:20:43 +0800

    test

--- guns-admin/src/main/webapp/static/js/common/mapConfig.js
+++ guns-admin/src/main/webapp/static/js/common/mapConfig.js
@@ -39,14 +39,38 @@ var mapConfig = {
         'base': [
             {
                 isShow: false,
+                id: 'source',
                 name: '水源工程',
                 url: 'http://localhost:6080/arcgis/rest/services/luhunguanqu/water_resouce/MapServer',
-                icon: 'icon-shuiyuan',
-                template: null,
+                icon: 'icon-shuiyuandi',
+                template: `<div class='popupContainer'>
+                                    <div class='popupHeader'>
+                                        <ul class='nav nav-tabs'>
+                                            <li class='active'>
+                                                <a href='#TableContent' data-toggle='tab'>{source_name}信息</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class='popupContent tab-content'>
+                                        <div class='popup-panel'>
+                                            <table>
+                                                <tr><th width="160">水源编码</th><td>{source_code}</td></tr>
+                                                <tr><th width="160">水源名称</th><td>{source_name}</td></tr>
+                                                <tr><th width="160">水源类型</th><td>{grade}</td></tr>
+                                                <tr><th width="160">正常蓄水位</th><td>{normal_level}米</td></tr>
+                                                <tr><th width="160">设计洪水位</th><td>{design_flood_level}米</td></tr>
+                                                <tr><th width="160">防洪控制水位</th><td>{flood_control_level}米</td></tr>
+                                                <tr><th width="160">总库容</th><td>{Total_capacity}立方米</td></tr>
+                                                <tr><th width="160">兴利库容</th><td>{beneficial_capacity}立方米</td></tr>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>`,
                 layer: null
             },
             {
                 isShow: false,
+                id: 'irr',
                 name: '灌区范围',
                 url: 'http://localhost:6080/arcgis/rest/services/luhunguanqu/water_resouce/MapServer',
                 icon: 'icon-guanqu',
@@ -55,6 +79,7 @@ var mapConfig = {
             },
             {
                 isShow: false,
+                id: 'canal',
                 name: '灌区渠道',
                 url: 'http://localhost:6080/arcgis/rest/services/luhunguanqu/water_resouce/MapServer',
                 icon: 'icon-qudao',
@@ -64,25 +89,50 @@ var mapConfig = {
         'build': [
             {
                 isShow: false,
-                name: '水源工程2',
-                url: 'http://localhost:6080/arcgis/rest/services/luhunguanqu/water_resouce/MapServer',
-                icon: 'icon-shuiyuan',
+                id: 'sluice',
+                name: '闸',
+                url: 'http://sampleserver6.arcgisonline.com/arcgis/rest/services/Hurricanes/MapServer/1',
+                icon: 'icon-zha',
                 template: null,
                 layer: null
             },
             {
                 isShow: false,
-                name: '灌区范围',
+                name: '涵',
                 url: 'http://localhost:6080/arcgis/rest/services/luhunguanqu/water_resouce/MapServer',
-                icon: 'icon-guanqu',
+                icon: 'icon-han',
                 template: null,
                 layer: null
             },
             {
                 isShow: false,
-                name: '灌区渠道',
+                name: '渡槽',
                 url: 'http://localhost:6080/arcgis/rest/services/luhunguanqu/water_resouce/MapServer',
-                icon: 'icon-qudao',
+                icon: 'icon-ducao',
+                template: null,
+                layer: null
+            },
+            {
+                isShow: false,
+                name: '倒虹吸',
+                url: 'http://localhost:6080/arcgis/rest/services/luhunguanqu/water_resouce/MapServer',
+                icon: 'icon-daohongxi',
+                template: null,
+                layer: null
+            },
+            {
+                isShow: false,
+                name: '桥',
+                url: 'http://localhost:6080/arcgis/rest/services/luhunguanqu/water_resouce/MapServer',
+                icon: 'icon-qiao',
+                template: null,
+                layer: null
+            },
+            {
+                isShow: false,
+                name: '其他建筑物',
+                url: 'http://localhost:6080/arcgis/rest/services/luhunguanqu/water_resouce/MapServer',
+                icon: 'icon-jianzhu',
                 template: null,
                 layer: null
             }]
